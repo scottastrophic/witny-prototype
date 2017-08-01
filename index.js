92c814e45ae6b5bf8fd07d5e4660b8479de9c54b
Riccardi, Ashley <ashley.riccardi@nytimes.com>
2017-08-01 10:57:24 -0400

    added template folder

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 1234
 
+import Home from './templates/home.html'
+
 const server = app.listen(port, () => {
   console.log('Server listening on port', port)
 })
 
 app.get('/', (req, res) => (
-  res.send('<div>HEYYYYYY</div>')
+  res.send(Home)
 ))
