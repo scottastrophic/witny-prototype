e0ebd1b615df9c8161c17f52be3594300f37e005
Riccardi, Ashley <ashley.riccardi@nytimes.com>
2017-08-01 11:01:43 -0400

    agh helo everyone

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 1234
 
-import Home from './templates/home.html'
+// import Home from './templates/home.html'
 
 const server = app.listen(port, () => {
   console.log('Server listening on port', port)
 })
 
 app.get('/', (req, res) => (
-  res.send(Home)
+  res.send('<div>Hello everyone</div>')
 ))
